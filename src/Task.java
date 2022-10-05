7c35dfdbe82775cf9b8ed8e333d74c5c20bc02ac
ALex Paltinean <paltinean.alexandru1@gmail.com>
2022-10-05 21:44:26 +0300

    Added some methods to add an remove

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,5 +20,47 @@ public class Task {
             Scanner input = new Scanner(System.in);
             answer = in.nextLine();
         }
+    }
+
+    public void removeTask(){
+        System.out.println("Do you want to remove a task? (y/n)");
+        Scanner in = new Scanner(System.in);
+        String answer = in.nextLine();
+        while(!answer.equals("n")){
+            System.out.println("Which task do you want to remove?");
+            int taskNumber = in.nextInt();
+            task.remove(taskNumber);
+            System.out.println("Do you want to remove a task? (y/n)");
+            in = new Scanner(System.in);
+            answer = in.nextLine();
+        }
+    }
+
+    public void checkTask(){
+        System.out.println("Do you finish a task? (y/n)");
+        Scanner in = new Scanner(System.in);
+        String answer = in.nextLine();
+        while(!answer.equals("n")){
+            System.out.println("Which task do you want to mark as done?");
+            int taskNumber = in.nextInt();
+            task.add(taskNumber, " Done!");
+            System.out.println("Do you want to remove a task? (y/n)");
+            in = new Scanner(System.in);
+            answer = in.nextLine();
+        }
+    }
+
+    public void saveTask(String name){
+        try(FileWriter fr = new FileWriter("TaskList" + name)){
+
+        }catch(IOException e){
+            System.out.println("The file couldn't be found!");
+
+        }
+    }
+
+    public void printTaskList(){
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
     }
 }
